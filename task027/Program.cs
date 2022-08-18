46d68e8653270bb152c24e3709ce2972fbc0e23b
FrilundAleksei <frilundag@gmail.com>
2022-08-18 17:55:36 +0300

    решены все 3 задачи

--- task027/Program.cs
+++ task027/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+using static System.Console; 
+Clear();
+
+  int Sum(int N)
+  {
+    int count = Convert.ToString(N).Length;
+    int var = 0;
+    int result = 0;
+        for (int i = 0; i < count; i++)
+        {
+          var = N - N % 10;
+          result = result + (N - var);
+          N = N / 10;
+        }
+        return result;
+  }
+
+Write("Введите число N: ");
+int N = Convert.ToInt32(ReadLine());
+int sum = Sum(N);
+WriteLine("Сумма цифр в числе: " + sum);
--- task029/Program.cs
+++ task029/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+using static System.Console; 
+Clear();
+
+//int [] numbers = new int[8];
+//Console.Write("[");
+//    for (int i = 0; i < numbers.Length; i++)
+//        {
+//         numbers [i] = new Random().Next();
+//        Console.Write(" " + Method (i) + " ");
+//       }
+//    Console.Write("]");
+
+
+
+//int Method (int a)
+//{
+//    return numbers[a];
+//}
+
+
+int [] NewArray = GetArrey(8);
+PrintArray(NewArray);
+WriteLine();
+
+
+
+int [] GetArrey (int N)
+{
+int [] array = new int[N];
+for (int i = 0;  i<N; i++)
+    {
+       array [i]= new Random().Next ();  
+    }
+       return array;
+       }
+
+void PrintArray(int[] Mass)
+{
+    Write("[ ");
+    for (int i = 0;  i<Mass.Length; i++)
+    {
+      Write (Mass[i] + ", ");
+    }
+    Write("]");
+}
